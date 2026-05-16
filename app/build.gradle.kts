c2eeed5879c79839ec80eb1196f931d7549a45f8
PARTE1 <mebarrera@uts.edu.co>
2026-05-15 21:04:24 -0500

    parte2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,16 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+
+    id("kotlin-kapt")
+
+
 }
 
 android {
     namespace = "com.torres.nodocivico"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
@@ -60,4 +64,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-}
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")}
--- app/src/main/java/com/torres/nodocivico/ui/reports/CreateReportFragment.kt
+++ app/src/main/java/com/torres/nodocivico/ui/reports/CreateReportFragment.kt
@@ -2,23 +2,48 @@ package com.torres.nodocivico.ui.reports
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.Toast
+import android.widget.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.torres.nodocivico.R
+import com.torres.nodocivico.data.local.database.AppDatabase
+import com.torres.nodocivico.data.local.entity.ReportEntity
+import kotlinx.coroutines.launch
 
 class CreateReportFragment : Fragment(R.layout.fragment_create_report) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+        val etTitle = view.findViewById<EditText>(R.id.etTitle)
+        val etDescription = view.findViewById<EditText>(R.id.etDescription)
         val btn = view.findViewById<Button>(R.id.btnSave)
 
+        val db = AppDatabase.getDatabase(requireContext())
+
         btn.setOnClickListener {
-            Toast.makeText(
-                requireContext(),
-                "Reporte guardado (simulado)",
-                Toast.LENGTH_SHORT
-            ).show()
+
+            val title = etTitle.text.toString()
+            val description = etDescription.text.toString()
+
+            if (title.isEmpty() || description.isEmpty()) {
+                Toast.makeText(requireContext(), "Campos obligatorios", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val report = ReportEntity(
+                title = title,
+                description = description,
+                category = "General",
+                priority = "Alta",
+                date = "2026-05-15",
+                status = "Pendiente"
+            )
+
+            lifecycleScope.launch {
+                db.reportDao().insertReport(report)
+
+                Toast.makeText(requireContext(), "Guardado en BD", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
